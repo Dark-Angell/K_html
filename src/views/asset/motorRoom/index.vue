9ea1597e9d6cd50a13e930458f6246cd4cfb03a6
fusheng <194485158@qq.com>
2025-09-22 18:39:47 +0800

    新增资产管理菜单栏

---
+++ src/views/asset/motorRoom/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    测试5
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
---
+++ src/views/asset/system/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    测试3
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
